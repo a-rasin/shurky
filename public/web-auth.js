1d3ba9ebc0d7e4e23372f6b4634aafa45b4ac270
anazir <anazir01@ucy.ac.cy>
2022-11-18 22:01:07 +0200

    Add basic webauthn

---
+++ public/web-auth.js
@@ -0,0 +1,164 @@
+const bufferToBase64 = buffer => btoa(String.fromCharCode(...new Uint8Array(buffer)));
+const base64ToBuffer = base64 => Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+
+const registerButton = document.querySelector('#register');
+const authenticateButton = document.querySelector('#authenticate');
+const deleteButton = document.querySelector('#delete');
+const loader = document.querySelector('#loader');
+
+const removeCredential = () => {
+  localStorage.removeItem('credential');
+  deleteButton.style.display = 'none';
+  authenticateButton.style.display = 'none';
+  registerButton.style.display = 'block';
+}
+
+const hasCredential = localStorage.getItem('credential') !== null;
+
+if(hasCredential) {
+  registerButton.style.display = 'none';
+  authenticateButton.style.display = 'block';
+  deleteButton.style.display = 'block';
+}
+else {
+  registerButton.style.display = 'block';
+}
+
+registerButton.addEventListener('click', async () => {
+  registerButton.disabled = true;
+  loader.innerHTML = 'Loading...';
+  loader.style.display = 'block';
+
+  try {
+    const credentialCreationOptions = await (await fetch(`/api/registration-options`, {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    })).json();
+
+    credentialCreationOptions.challenge = new Uint8Array(credentialCreationOptions.challenge.data);
+    credentialCreationOptions.user.id = new Uint8Array(credentialCreationOptions.user.id.data);
+    credentialCreationOptions.user.name = 'test@test.com';
+    credentialCreationOptions.user.displayName = 'Test account';
+    credentialCreationOptions.authenticatorSelection = {};
+
+    const credential = await navigator.credentials.create({
+      publicKey: credentialCreationOptions
+    });
+
+    const credentialId = bufferToBase64(credential.rawId);
+
+    localStorage.setItem('credential', JSON.stringify({credentialId}));
+
+    console.log(bufferToBase64(credential.response.attestationObject))
+    const data = {
+      rawId: credentialId,
+      response: {
+        attestationObject: bufferToBase64(credential.response.attestationObject),
+        clientDataJSON: bufferToBase64(credential.response.clientDataJSON),
+        id: credential.id,
+        type: credential.type
+      }
+    };
+
+    await (await fetch(`/api/register`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({credential: data}),
+      credentials: 'include'
+    })).json();
+
+    registerButton.style.display = 'none';
+    authenticateButton.style.display = 'block';
+    deleteButton.style.display = 'block';
+
+    loader.innerHTML = 'Registration successful!';
+
+  } catch (err) {
+    console.error('registration failed', err);
+
+    loader.innerHTML = 'Registration failed';
+  } finally {
+    registerButton.disabled = false;
+    loader.style.display = 'none';
+  }
+})
+
+authenticateButton.addEventListener('click', async () => {
+  authenticateButton.disabled = true;
+  deleteButton.disabled = true;
+  loader.innerHTML = 'Loading...';
+  loader.style.display = 'block';
+
+  try {
+    const credentialRequestOptions = await (await fetch(`/api/authentication-options`, {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    })).json();
+
+    const {credentialId} = JSON.parse(localStorage.getItem('credential'));
+
+    credentialRequestOptions.challenge = new Uint8Array(credentialRequestOptions.challenge.data);
+    credentialRequestOptions.allowCredentials = [
+      {
+        id: base64ToBuffer(credentialId),
+        type: 'public-key',
+        transports: [ "usb", "nfc", "ble", "hybrid", "internal" ]
+      }
+    ];
+
+    const credential = await navigator.credentials.get({
+      publicKey: credentialRequestOptions
+    });
+
+    const data = {
+      rawId: bufferToBase64(credential.rawId),
+      response: {
+        authenticatorData: bufferToBase64(credential.response.authenticatorData),
+        signature: bufferToBase64(credential.response.signature),
+        userHandle: bufferToBase64(credential.response.userHandle),
+        clientDataJSON: bufferToBase64(credential.response.clientDataJSON),
+        id: credential.id,
+        type: credential.type
+      }
+    };
+
+    const response = (await fetch(`/api/authenticate`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({credential: data}),
+      credentials: 'include'
+    }));
+
+    if(response.status === 401) {
+      loader.innerHTML = 'Credential has expired, please register a new credential';
+
+      removeCredential();
+    } else {
+      await response.json();
+
+      loader.innerHTML = 'Authentication successful!';
+    }
+  } catch(e) {
+    console.error('authentication failed', e);
+
+    loader.innerHTML = 'Authentication failed';
+  } finally {
+    authenticateButton.disabled = false;
+    deleteButton.disabled = false;
+    loader.style.display = 'none';
+  }
+});
+
+deleteButton.addEventListener('click', removeCredential);
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const crypto = require('crypto');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const mysql = require('mysql');
+const base64url = require('base64url');
+const { Fido2Lib } = require("fido2-lib");
 dotenv.config();
 var pool  = mysql.createPool({
   connectionLimit: process.env.DB_MAX_CON,
@@ -9,6 +12,15 @@ var pool  = mysql.createPool({
   user           : process.env.DB_USER,
   password       : process.env.DB_PSWD,
   database       : process.env.DB_NAME,
+});
+
+const f2l = new Fido2Lib({
+    challengeSize: 128,
+    attestation: "none",
+    cryptoParams: [-7, -257],
+    authenticatorAttachment: "platform",
+    authenticatorRequireResidentKey: false,
+    authenticatorUserVerification: "required"
 });
 
 const app = express();
@@ -53,7 +65,6 @@ app.get('/api/data', async (_, res) => {
   try {
     const { results } = await query('SELECT * FROM BOOKS');
     // const { results } = await query('SELECT * FROM BOOKS WHERE id = ?', [1]);
-    console.log(results)
     res.send(results.map((a) => ({ id: a.id, data: [ a.sales, a.title, a.author, a.price ]})));
   } catch (err) {
     res.status(500).send(err);
@@ -90,7 +101,6 @@ app.get('/api/user-types', async (req, res) => {
 app.get('/api/phone-lookup/:phone', async (req, res) => {
   try {
     const { results } = await query('SELECT * FROM USERS WHERE PHONE = ?', [req.params.phone]);
-    console.log(results)
     res.send({ exists: results.length > 0, ...results[0] });
   } catch (err) {
     res.status(500).send(err);
@@ -112,6 +122,92 @@ app.post('/api/create-user/', async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+app.get('/api/registration-options', async (req, res) => {
+  const registrationOptions = await f2l.attestationOptions();
+
+  req.session.challenge = Buffer.from(registrationOptions.challenge);
+  req.session.userHandle = crypto.randomBytes(32);
+
+  registrationOptions.user.id = req.session.userHandle;
+  registrationOptions.challenge = Buffer.from(registrationOptions.challenge);
+
+  // iOS
+  // registrationOptions.authenticatorSelection = {authenticatorAttachment: 'platform'};
+
+  res.json(registrationOptions);
+});
+
+app.get('/api/authentication-options', async (req, res) => {
+  const authnOptions = await f2l.assertionOptions();
+
+  req.session.challenge = Buffer.from(authnOptions.challenge);
+  authnOptions.challenge = Buffer.from(authnOptions.challenge);
+
+  res.json(authnOptions);
+})
+
+app.post('/api/register', async (req, res) => {
+  const {credential} = req.body;
+
+  const challenge = new Uint8Array(req.session.challenge.data).buffer;
+  credential.rawId = new Uint8Array(Buffer.from(credential.rawId, 'base64')).buffer;
+  credential.response.attestationObject = base64url.decode(credential.response.attestationObject, 'base64');
+  credential.response.clientDataJSON = base64url.decode(credential.response.clientDataJSON, 'base64');
+
+  const attestationExpectations = {
+    challenge,
+    origin: 'http://localhost:5000',
+    factor: 'either'
+  };
+
+  try {
+    const regResult = await f2l.attestationResult(credential, attestationExpectations);
+
+    req.session.publicKey = regResult.authnrData.get('credentialPublicKeyPem');
+    req.session.prevCounter = regResult.authnrData.get('counter');
+
+    res.json({status: 'ok'});
+  }
+  catch(e) {
+    console.log('error', e);
+    res.status(500).json({status: 'failed'});
+  }
+});
+
+app.post('/api/authenticate', async (req, res) => {
+  const {credential} = req.body;
+
+  credential.rawId = new Uint8Array(Buffer.from(credential.rawId, 'base64')).buffer;
+
+  const challenge = new Uint8Array(req.session.challenge.data).buffer;
+  const {publicKey, prevCounter} = req.session;
+
+  if(publicKey === 'undefined' || prevCounter === undefined) {
+    res.status(401).json({status: 'non authorized'});
+  }
+  else {
+    const assertionExpectations = {
+      challenge,
+      origin: 'http://localhost:5000',
+      factor: 'either',
+      publicKey,
+      prevCounter,
+      userHandle: new Uint8Array(Buffer.from(req.session.userHandle, 'base64')).buffer  //new Uint8Array(Buffer.from(req.session.userHandle.data)).buffer
+    };
+
+    try {
+      await f2l.assertionResult(credential, assertionExpectations); // will throw on error
+
+      res.json({status: 'ok'});
+    }
+    catch(e) {
+      console.log('error', e);
+      res.status(500).json({status: 'failed'});
+    }
+  }
+});
+
 
 const query = (query, extra) => (new Promise((resolve, reject) => {
   pool.query(query, extra, function (error, results, fields) {
